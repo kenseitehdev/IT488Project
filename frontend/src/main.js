8b8d1607eab4b192d023e24b389b7c5ed38accfc
Kensei <tehkensei@gmail.com>
2026-01-12 13:43:04 -0600

    replaced vite default queryselector with custom one

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,21 +3,11 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import counter from '../components/counter.js'
-document.querySelector('#app').innerHTML = `
+$.selector("#app").html(`
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello World, Frontend</h1>
     <div class="card">
     <counter-box></counter-box>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
-`
-
+`);
