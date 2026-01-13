5ec781179a3c07b84e846d7d1e3a71bdf37d6dbd
Kensei <tehkensei@gmail.com>
2026-01-13 14:23:23 -0600

    added sample item card

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,14 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import counter from './components/counter.js'
+import card from './components/card.js'
+
 $.selector("#app").html(`
   <div>
     <h1 class="text-blue-500">Hello World, Frontend</h1>
     <div class="card">
     <counter-box></counter-box>
+    <item-card></item-card>
     </div>
   </div>
 `);
