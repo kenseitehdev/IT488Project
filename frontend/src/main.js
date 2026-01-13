77ca1dfc38a73e03c394080c5034b70e67869bca
Kensei <tehkensei@gmail.com>
2026-01-13 12:56:42 -0600

    added tailwind

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import viteLogo from '/vite.svg'
 import counter from '../components/counter.js'
 $.selector("#app").html(`
   <div>
-    <h1>Hello World, Frontend</h1>
+    <h1 class="text-blue-500">Hello World, Frontend</h1>
     <div class="card">
     <counter-box></counter-box>
     </div>
