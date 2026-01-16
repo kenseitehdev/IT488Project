a72820e253eaa8abec0c2df05d0da1789145e6d5
Kensei <tehkensei@gmail.com>
2026-01-15 19:45:22 -0600

    updaated frontend, removed placeholder header and counter, updated nav username

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import './Vistera/VistraJS.js'
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import counter from './components/counter.js'
 import card from './components/card.js'
 import nav from './components/nav.js'
 import footer from './components/footer.js'
@@ -11,8 +10,6 @@ $.selector("#app").html(`
   <div>
     <nav-bar></nav-bar>
     <div class="content mx-8">
-    <h1 class="text-blue-500">Hello World, Frontend</h1>
-    <counter-box></counter-box>
     <item-card></item-card>
     </div>
     <footer-bar></footer-bar>
